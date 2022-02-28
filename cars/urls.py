7809fcfddf48090f0323157f2d102b506e9d0804
lukas <lukas.durinak@gmail.com>
2022-02-28 19:32:59 +0100

    car views updated templates update

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,8 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detail, name='car_detail'),
 ]
